7968685d6abb0cdb9dfd0a02b12517ca079df4cd
VictoriaSzy <vszymanowski00@stuy.edu>
2019-02-25 11:34:49 -0500

    Added Mr. K's test and ran it - success!

--- QueenBoard.java
+++ QueenBoard.java
@@ -3,6 +3,11 @@ public class QueenBoard {
   private int[][] board ;
 
   public static void main(String[] args) {
+    System.out.println("We are going to run the test!") ;
+    for (int i = 0 ; i < 6 ; i++) {
+      System.out.println("The current index is " + i) ;
+      runTest(i) ;
+    }
     // I used this visualizer to also help me debug: http://pythontutor.com/visualize.html#mode=edit
     /*System.out.println("*********************************************************************************************************************") ;
     System.out.println("*********************************************************************************************************************") ;
@@ -259,6 +264,26 @@ public class QueenBoard {
     }
     //System.out.println("\nThe method countH() has concluded!") ;
     return total ;
+  }
+
+  ////////////////////////////////////////////////////////////////////////////////////
+  //testcase must be a valid index of your input/output array
+  public static void runTest(int i){
+    QueenBoard b;
+    int[]tests =   {1,2,3,4,5,8};
+    int[]answers = {1,0,0,2,10,92};
+    if(i >= 0 && i < tests.length ){
+      int size = tests[i];
+      int correct = answers[i];
+      b = new QueenBoard(size);
+      int ans  = b.countSolutions();
+
+      if(correct==ans){
+        System.out.println("PASS board size: "+tests[i]+" "+ans);
+      }else{
+        System.out.println("FAIL board size: "+tests[i]+" "+ans+" vs "+correct);
+      }
+    }
   }
 
 }
